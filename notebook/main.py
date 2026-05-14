5347f1bfd8da7ec5f83cb1e355319be510311369
Ainara-1 <vilanuccii@yahoo.com>
2026-05-14 03:58:18 +0200

    prueba6

--- notebook/main.py
+++
@@ -1,9 +0,0 @@
-# importando librerias
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-
-datos=pd.read_csv("../data/Teen_Mental_Health_Dataset.csv")
-datos.info()
